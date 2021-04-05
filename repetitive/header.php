c4a3fc897731bd1cba253fb224be9d955b643653
ganeaflorin <ganeafloman@gmail.com>
2021-04-05 17:41:09 +0300

    switched logo to skivi's head

--- repetitive/header.php
+++ repetitive/header.php
@@ -1,22 +1,30 @@
 <style>
-<?php include "../repetitive/header.css"; ?> 
+    <?php include "../repetitive/header.css"; ?>
 </style>
 <header>
-        <div class="logo">
-            <a href="../landing/landing.php" class="logo__link">SkiVI </a>
+    <a href="../landing/landing.php">
+    <div class="logo">
+        <div class="head">
+            <div class="face">
+                <div class="eye"> </div>
+                <div class="eye"> </div>
+            </div>
+            </div>
         </div>
-        <nav>
-            <div class="nav-button">
-                <a href="../login/login.php" class="nav-button__anchor">Login</a>
-            </div>
-            <div class="nav-button">
-                <a href="../register/register.php" class="nav-button__anchor">Sign Up</a>
-            </div>
-            <div class="nav-button">
-                <a href="../paths/paths.php" class="nav-button__anchor">Paths</a>
-            </div>
-            <div class="nav-button">
-                <a href="../about/about.php" class="nav-button__anchor">About</a>
-            </div>
-        </nav>
-    </header>
+    </a>
+
+    <nav>
+        <div class="nav-button">
+            <a href="../login/login.php" class="nav-button__anchor">Login</a>
+        </div>
+        <div class="nav-button">
+            <a href="../register/register.php" class="nav-button__anchor">Sign Up</a>
+        </div>
+        <div class="nav-button">
+            <a href="../paths/paths.php" class="nav-button__anchor">Paths</a>
+        </div>
+        <div class="nav-button">
+            <a href="../about/about.php" class="nav-button__anchor">About</a>
+        </div>
+    </nav>
+</header>
