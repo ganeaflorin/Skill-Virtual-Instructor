ef7c469d6d0f21b68d21706ca9fd8f2da4cde675
ganeaflorin <ganeafloman@gmail.com>
2021-04-07 13:15:47 +0300

    created profile+reset-pass

---
+++ profile/profile.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkiVI - Profile</title>
+    <style>
+        <?php
+        include "../profile/profile.css";
+        ?>
+    </style>
+</head>
+
+<body>
+    <?php include "../repetitive/header.php"; ?>
+
+
+
+    <main>
+
+        <div class="profile">
+            <div class="profile__infos">
+                <div class="profile__infos__avatar">
+                    <img src="../images/avatar.png" alt="user avatar" class="user-avatar">
+                    <button id="change-avatar" class="change-avatar"> Change avatar</button>
+                </div>
+                <div class="profile__infos__credentials">
+                    <div class="profile__infos__credentials__field">
+                        Username: lorem ipsum
+                    </div>
+                    <div class="profile__infos__credentials__field">
+                        Email: lorem@ipsum.com
+                    </div>
+
+                    <button onClick="location.href='../reset-password/reset-password.php'" id="change-pass"  class="change-pass">
+                            Change password </button>
+                    
+
+                </div>
+            </div>
+
+            <div class="profile__skills">
+                <div class="skill">
+                    <p>Origami</p>
+                    <div class="skill__bar">
+                        <div class="skill__bar__default-progress origami-progress">
+                            <p class="progress-percent"> 80%</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <p>Tie knot</p>
+                    <div class="skill__bar">
+                        <div class="skill__bar__default-progress tie-progress">
+                            <p class="progress-percent"> 60%</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <p>Fast meals</p>
+                    <div class="skill__bar">
+                        <div class="skill__bar__default-progress meals-progress">
+                            <p class="progress-percent"> 10%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </main>
+
+
+
+
+
+    <?php include "../repetitive/footer.php"; ?>
+</body>
+
+</html>
---
+++ reset-password/reset-password.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkiVI - Reset password</title>
+    <style> 
+    <?php include "reset-password.css" ?>
+    </style>
+</head>
+
+<body>
+<?php 
+    include("../repetitive/header.php");
+    ?>
+
+    <main>
+
+        <div class="login-form">
+            <h1 class="login-form__h1">Reset password</h1>
+            <form>
+                <input type="password" name="current-password" placeholder="Current password" class="form-field" required>
+                <input type="password" name="new-password" placeholder="New password" class="form-field" required>
+                <button type="submit" name="reset-password" class="login-btn">Reset</button>
+            </form>
+           
+        </div>
+
+    </main>
+
+    <?php 
+    include("../repetitive/footer.php");
+    ?>
+</body>
+
+</html>
